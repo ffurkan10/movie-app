a2324b5945f8526e4fde0b16420cac6d262733d9
Fatih Furkan Kocabiyik <82720725+ffurkan10@users.noreply.github.com>
2022-08-28 15:00:50 +0300

    add request to app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
 import "./styles/style.scss";
-import { Create, MovieList, Navbar } from "./components/index";
+import { Navbar } from "./components/index";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MovieDetails from "./pages/moviedetails/MovieDetails";
+import Home from "./pages/home/Home";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [detail, setDetail] = useState([]);
+  const [page, setPage] = useState(1);
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=ccb0a8566b23ab43471cda53fed3d9e7&language=en-US&page=${page}`
+      )
+      .then((res) => {
+        setDetail(res.data.results);
+      });
+  }, []);
+  console.log(detail);
+  const nextClick = () => {
+    setPage((page) => page + 1);
+  };
+
+  const prevClick = () => {
+    setPage((page) => page - 1);
+  };
   return (
     <Router>
       <div className="app">
         <Navbar />
         <div className="app__list">
           <Routes>
-            <Route path="/" element={<MovieList />} />
-            <Route path="/movie/:id" element={<Create />} />
+            <Route
+              path="/"
+              element={
+                <Home
+                  detail={detail}
+                  nextClick={nextClick}
+                  prevClick={prevClick}
+                  setDetail={setDetail}
+                />
+              }
+            />
+            <Route path="/movie/:id" element={<MovieDetails />} />
           </Routes>
         </div>
       </div>
